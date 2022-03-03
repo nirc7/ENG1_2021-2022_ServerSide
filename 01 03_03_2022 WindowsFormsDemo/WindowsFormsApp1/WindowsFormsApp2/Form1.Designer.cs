d0ea57b9e742638dcab368d86bb716d499a0f00d
LabM RUPPIN <labm@ruppin.ac.il>
2022-03-03 16:05:29 +0200

    files

---
+++ 01 03_03_2022 WindowsFormsDemo/WindowsFormsApp1/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWirte = new System.Windows.Forms.Button();
+            this.btnreadAlltext = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReactLinmes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnWirte
+            // 
+            this.btnWirte.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnWirte.Location = new System.Drawing.Point(106, 58);
+            this.btnWirte.Name = "btnWirte";
+            this.btnWirte.Size = new System.Drawing.Size(94, 30);
+            this.btnWirte.TabIndex = 0;
+            this.btnWirte.Text = "Wirte";
+            this.btnWirte.UseVisualStyleBackColor = true;
+            this.btnWirte.Click += new System.EventHandler(this.btnWirte_Click);
+            // 
+            // btnreadAlltext
+            // 
+            this.btnreadAlltext.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnreadAlltext.Location = new System.Drawing.Point(106, 110);
+            this.btnreadAlltext.Name = "btnreadAlltext";
+            this.btnreadAlltext.Size = new System.Drawing.Size(94, 30);
+            this.btnreadAlltext.TabIndex = 1;
+            this.btnreadAlltext.Text = "ReadAllText";
+            this.btnreadAlltext.UseVisualStyleBackColor = true;
+            this.btnreadAlltext.Click += new System.EventHandler(this.btnreadAlltext_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(102, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // btnReactLinmes
+            // 
+            this.btnReactLinmes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnReactLinmes.Location = new System.Drawing.Point(106, 159);
+            this.btnReactLinmes.Name = "btnReactLinmes";
+            this.btnReactLinmes.Size = new System.Drawing.Size(94, 30);
+            this.btnReactLinmes.TabIndex = 3;
+            this.btnReactLinmes.Text = "ReadAllLines";
+            this.btnReactLinmes.UseVisualStyleBackColor = true;
+            this.btnReactLinmes.Click += new System.EventHandler(this.btnReactLinmes_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 625);
+            this.Controls.Add(this.btnReactLinmes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnreadAlltext);
+            this.Controls.Add(this.btnWirte);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWirte;
+        private System.Windows.Forms.Button btnreadAlltext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReactLinmes;
+    }
+}
+
---
+++ 01 03_03_2022 WindowsFormsDemo/WindowsFormsApp1/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        string filePath = @"./MyFiles/MyData.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWirte_Click(object sender, EventArgs e)
+        {
+            File.AppendAllText(filePath, "hello dora\n");
+            ShowTableFromTxt();
+        }
+
+        private void btnreadAlltext_Click(object sender, EventArgs e)
+        {
+            label1.Text = File.ReadAllText(filePath);
+        }
+
+        private void btnReactLinmes_Click(object sender, EventArgs e)
+        {
+            ShowTableFromTxt();
+        }
+
+        private void ShowTableFromTxt()
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            int counter = 1;
+            label1.Text = "";
+
+            foreach (var line in lines)
+            {
+                label1.Text += counter++ + ". " + line + "\n";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowTableFromTxt();
+        }
+    }
+}
