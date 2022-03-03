547d6cd0d477d34f0419a337d9ebc0ed59ddc704
LabM RUPPIN <labm@ruppin.ac.il>
2022-03-03 14:48:53 +0200

    added GUI

---
+++ 01 03_03_2022 WindowsFormsDemo/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSayHello = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // btnSayHello
+            // 
+            this.btnSayHello.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSayHello.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnSayHello.Location = new System.Drawing.Point(111, 51);
+            this.btnSayHello.Name = "btnSayHello";
+            this.btnSayHello.Size = new System.Drawing.Size(100, 38);
+            this.btnSayHello.TabIndex = 0;
+            this.btnSayHello.Text = "Say Hello";
+            this.btnSayHello.UseVisualStyleBackColor = false;
+            this.btnSayHello.Click += new System.EventHandler(this.btnSayHello_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.txtName.Location = new System.Drawing.Point(111, 95);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 26);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblResult.Location = new System.Drawing.Point(111, 138);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(21, 20);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "...";
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btn2.Location = new System.Drawing.Point(273, 51);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(100, 38);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "btn2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            this.btn2.MouseEnter += new System.EventHandler(this.btn2_MouseEnter);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(115, 219);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 4;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "One",
+            "Two",
+            "Three"});
+            this.comboBox1.Location = new System.Drawing.Point(406, 219);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(510, 95);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSayHello);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSayHello;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ 01 03_03_2022 WindowsFormsDemo/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSayHello_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            name = "Hello " + name;
+            lblResult.Text = name;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = comboBox1.SelectedIndex + ", " + comboBox1.SelectedItem;
+        }
+
+        private void btn2_MouseEnter(object sender, EventArgs e)
+        {
+            btn2.BackColor = Color.Green;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //lblResult.Text = comboBox1.SelectedIndex + ", " + comboBox1.SelectedItem;
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            lblResult.Text = monthCalendar1.SelectionStart.ToShortDateString() + ", " + 
+                DateTime.Now.ToLongTimeString();
+
+            DateTime dt = new DateTime(2020,7,7);
+        }
+    }
+}
