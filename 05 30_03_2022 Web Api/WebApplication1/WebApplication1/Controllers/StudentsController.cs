a2b940c1e2dcc6f6ef5917ca8b0fa288cf79f966
nc <31476486+nirc7@users.noreply.github.com>
2022-03-30 17:38:02 +0300

    webapi2

---
+++ 05 30_03_2022 Web Api/WebApplication1/WebApplication1/Controllers/StudentsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        public IEnumerable<Student> Get() 
+        {
+            return StudentsDBMock.students.ToArray();
+        }
+
+        public Student Get(int id)
+        {
+            return StudentsDBMock.students.FirstOrDefault(stu=> stu.ID == id);
+        }
+
+        public int Post([FromBody] Student value) 
+        {
+            int max = StudentsDBMock.students.Max(stu=> stu.ID);
+            value.ID = max + 1;
+            StudentsDBMock.students.Add(value);
+            return value.ID;
+        }
+
+        public string Put(int id, [FromBody] Student value) 
+        {
+            Student studetn2update = StudentsDBMock.students.FirstOrDefault(stu=> stu.ID== id);
+            studetn2update.Name = value.Name;
+            studetn2update.Grade = value.Grade;
+            return "done:)";
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            Student student2Delete = StudentsDBMock.students.SingleOrDefault(stu=> stu.ID==id);
+            StudentsDBMock.students.Remove(student2Delete);
+            var jRes = new { Result="deleted successfully!"};
+            var res = Json(jRes);
+            return res;
+        }
+
+    }
+}
---
+++ 05 30_03_2022 Web Api/WebApplication1/WebApplication1/Models/StudentsDBMock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public static class StudentsDBMock
+    {
+        public static List<Student> students = new List<Student>() {
+            new Student(){ID = 1, Name= "avi" , Grade = 99},
+            new Student(){ID = 3, Name= "charlie" , Grade = 100},
+            new Student(){ID = 4, Name= "benny" , Grade = 95},
+            new Student(){ID = 2, Name= "dora" , Grade = 97},
+        };
+    }
+}
